b2878d7b81e9ea0f1c1fbbd99a9b2afe7853651f
evidad <errolvidad@gmail.com>
2024-11-17 18:33:39 -0800

    Redirect root URL to login

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,33 +1,18 @@
-"""
-URL configuration for django_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from kakeibo_finance.views import CustomLoginView, SignupView, home, dashboard, add_transaction, edit_transaction, delete_transaction, add_budget, edit_budget, category_list, add_category, edit_category, delete_category
+from kakeibo_finance.views import CustomLoginView, SignupView, dashboard, add_transaction, edit_transaction, delete_transaction, add_budget, edit_budget, category_list, add_category, edit_category, delete_category
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView  # Add the LogoutView import
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.shortcuts import redirect  # Import redirect
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),  # Add logout URL
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('signup/', SignupView.as_view(), name='signup'),
-    path('', home, name='home'),
+    path('', lambda request: redirect('login'), name='home'),  # Redirect root URL to login
     path('dashboard/', dashboard, name='dashboard'),
     path('password_change/', PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
